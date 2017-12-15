930bfc98210ed305f87c98b7c7fd62205d3fba5b
Redocram <redocram@gmail.com>
2017-12-15 12:31:05 +0100

    Login and logout done

--- Antonino/Controllers/AccountController.cs
+++ Antonino/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Antonino.Controllers
 {
@@ -44,6 +45,13 @@ namespace Antonino.Controllers
             {
                 return View();
             }
+        }
+
+        [Authorize]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
